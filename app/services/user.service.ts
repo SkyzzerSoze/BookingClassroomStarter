aa9923e5ae307919cf3cdfee2b4d8262ea9d23e2
Natacha2810Chihiro <nat.tgr2@gmail.com>
2025-05-19 13:02:28 +0200

    Niveau 5

--- app/services/user.service.ts
+++ app/services/user.service.ts
@@ -1,17 +1,23 @@
 import api from "./api.service";
 
+const updateUser = async (data: any) => {
+  const response = await api.put("/auth/me", data);
+  return response.data;
+};
+
 const getUserWithToken = async (token: string) => {
-  const reponse = await api.get("/users/me", {
+  const response = await api.get("/users/me", {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
 
-  return reponse.data;
+  return response.data;
 };
 
 const UserService = {
   getUserWithToken,
+  updateUser,
 };
 
 export default UserService;
